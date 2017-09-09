3f8a7a8ca17c4fafc6210807279b88dbe4a1bf8e
geoiraq <george.ejaam@gmail.com>
2017-09-09 21:22:53 +1000

    adding detect code function

--- detectcode.py
+++ detectcode.py
@@ -1,3 +1,11 @@
+''' 
+Usage
+detectCode('<p> somthing <code> some code! </code> </p>')
+returns
+[<code> some code! </code>]
+if more code exists return them all 
+if none return none!
+'''
 from sys import argv
 from bs4 import BeautifulSoup
 def detectCode(*args):
