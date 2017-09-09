2b0114ec94195c9f7386a1ede372de4ddfbfbe72
geoiraq <george.ejaam@gmail.com>
2017-09-09 21:20:53 +1000

    adding detect code function

--- detectcode.py
+++ detectcode.py
@@ -1,6 +1,13 @@
-import BeautifulSoup
-soup = BeautifulSoup(argv[1])
-for el in soup.findall('code'):
-	print el.code.string
+from sys import argv
+from bs4 import BeautifulSoup
+def detectCode(*args):
+	soup = BeautifulSoup(argv[1])
+	result = soup.find_all(lambda tag: tag.name == 'code')
+	if not result:
+		print "none"
+		return False
+	else:
+		print result
+		return result
 
-
+detectCode(argv[1])
