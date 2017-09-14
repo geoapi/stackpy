f6f6455cb1f4de5068e5b129816b04c912c5cc44
geoiraq <george.ejaam@gmail.com>
2017-09-14 15:05:58 +1000

    updates

--- detectcode.py
+++ detectcode.py
@@ -8,15 +8,15 @@ if none return none!
 '''
 from sys import argv
 from bs4 import BeautifulSoup
+codeList = [];
 def detectCode(code, *args):
 	soup = BeautifulSoup(code, "lxml")
 	result = soup.find_all(lambda tag: tag.name == 'code')
 	if not result:
-		print "none"
-		return False
+		print "none"		
 	else:
 		print result
-		return result
+	return result
 print argv[1]
 
 detectCode(argv[1])
--- getquestion.py
+++ getquestion.py
@@ -1,6 +1,8 @@
 import requests
 from sys import argv
-
+'''
+give a search query or keyword and title or body
+'''
 def getQuestions(query, *args):
 	response = requests.get("https://api.stackexchange.com/2.2/search/advanced?order=desc&sort=votes&accepted=true&tagged=api&filter=withbody&site=stackoverflow"+"&q="+query)
 	jsonobj = response.json()
